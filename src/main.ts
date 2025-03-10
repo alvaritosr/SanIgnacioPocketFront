94270c273a46a9481a4469aa4c0bb4b917af9d89
jcanterla <jcanterlarufino@safareyes.es>
2025-03-10 16:24:19 +0100

    carga de imagenes prueba 2

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
 import { importProvidersFrom } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http'; // <-- Importa HttpClientModule
+import {HttpClientModule, provideHttpClient} from '@angular/common/http'; // <-- Importa HttpClientModule
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
@@ -11,6 +11,7 @@ bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
+    provideHttpClient(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
     importProvidersFrom(HttpClientModule),
   ],
