58a536aaa24daa83a94b431e197001fb8046a2a8
jcanterla <jcanterlarufino@safareyes.es>
2025-03-10 13:14:41 +0100

    base de datos cartas

---
+++ src/estampitas_202503101312.sql
@@ -0,0 +1,22 @@
+INSERT INTO sanignaciopocket.estampitas (imagen,nombre) VALUES
+	 ('https://upload.wikimedia.org/wikipedia/commons/thumb/0/00/Beltr%C3%A1n_from_Nacimiento_de_San_Ignacio_de_Loyola_y_profec%C3%ADas_que_le_predijeron_%28cropped%29.jpg/175px-Beltr%C3%A1n_from_Nacimiento_de_San_Ignacio_de_Loyola_y_profec%C3%ADas_que_le_predijeron_%28cropped%29.jpg','Beltrán Yáñez de Oñaz y Loyola'),
+	 ('https://upload.wikimedia.org/wikipedia/commons/thumb/b/b0/Mar%C3%ADa_from_Nacimiento_de_San_Ignacio_de_Loyola_y_profec%C3%ADas_que_le_predijeron_%28cropped%29.jpg/220px-Mar%C3%ADa_from_Nacimiento_de_San_Ignacio_de_Loyola_y_profec%C3%ADas_que_le_predijeron_%28cropped%29.jpg','Marina Sáenz de Licona y Balda'),
+	 ('https://historia-hispanica.rah.es/api/images/biographies/582-Adriano-VI-.jpg','Antonio Manrique de Lara'),
+	 ('https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Portrait_of_Charles_V%2C_Holy_Roman_Emperor%2C_seated_%281500%E2%80%931558%29%2C_formerly_attributed_to_Titian_%28Alte_Pinakothek%2C_Munich%29.jpg/245px-Portrait_of_Charles_V%2C_Holy_Roman_Emperor%2C_seated_%281500%E2%80%931558%29%2C_formerly_attributed_to_Titian_%28Alte_Pinakothek%2C_Munich%29.jpg','Carlos I de España'),
+	 ('https://cloudfront-us-east-1.images.arcpublishing.com/infobae/7H5LX677TRBVVNTHF5LT7UMYW4.png','Pedro de Mendoza'),
+	 ('https://www.exaudi.org/wp-content/uploads/2021/11/Disen%CC%83o-sin-ti%CC%81tulo-41.jpg','San Francisco de Javier'),
+	 ('https://www.salvadmereina.org/wp-content/uploads/2013/12/3_85d8c63b32f3c4f5d9be13cbdb307c70.jpg','San Pedro Fabro'),
+	 ('https://upload.wikimedia.org/wikipedia/commons/b/b1/Portret_van_de_jezu%C3%AFet_Jacobus_Laynez_Portretten_van_generaal_oversten_van_de_jezu%C3%AFeten_%28serietitel%29_Effigies_pr%C3%A6positorvm_generalivm_societatis_Iesv_%28serietitel%29%2C_RP-P-1910-4016.jpg','Diego Laínez'),
+	 ('https://www.academiacolecciones.com/dibujos/server/files/DG-0088.jpg','Alfonso Salmerón'),
+	 ('https://lugares.inah.gob.mx/images/piezas/_thumb1/10-6747.jpg','Nicolás de Bobadilla');
+INSERT INTO sanignaciopocket.estampitas (imagen,nombre) VALUES
+	 ('https://upload.wikimedia.org/wikipedia/commons/4/4a/Anonimo_010.JPG','Simón Rodrigues'),
+	 ('','Juan de Polanco'),
+	 ('','Juan de Velázquez'),
+	 ('https://upload.wikimedia.org/wikipedia/commons/5/5e/Anonimo-jeronimo_nadal.jpg','Jerónimo Nadal'),
+	 ('https://upload.wikimedia.org/wikipedia/commons/d/d4/Portrait_of_Pope_Paul_III_Farnese_%28by_Titian%29_-_National_Museum_of_Capodimonte.jpg','Papa Paulo III'),
+	 ('https://upload.wikimedia.org/wikipedia/commons/thumb/7/72/Portrait_of_Pope_Adrian_VI_%28after_Jan_van_Scorel%29.jpg/1200px-Portrait_of_Pope_Adrian_VI_%28after_Jan_van_Scorel%29.jpg','Papa Adriano VI'),
+	 ('https://upload.wikimedia.org/wikipedia/commons/5/52/San_Francisco_de_Borja.jpg','San Francisco de Borja'),
+	 ('https://upload.wikimedia.org/wikipedia/commons/7/76/D._Jo%C3%A3o_III_1502-1557_hd.jpg','Juan III de Portugal'),
+	 ('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEfU4EPnKvVZfXNpsbVuqbyak1Ym4cV2TRUw&s','Isabel Roser'),
+	 ('https://upload.wikimedia.org/wikipedia/commons/d/d1/Alexander_VII.jpg','Papa Alejandro VII (Fabio Chigi)');
